81643fbd2d19377def87e0fa0989ad33b256e959
IlyaSevrin <i.sevrin@yandex.ru>
2022-10-10 19:54:40 +0500

    jsCommit0.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personaMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,12 +9,24 @@ const personaMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?'),
-      d = prompt('На сколько вы оцените его?');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов'),
+          b = prompt('На сколько оцените его?');
+    
+    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != ''){
+        personaMovieDB.movies[a] = b;        
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personaMovieDB.movies[a] = b;
-personaMovieDB.movies[c] = d;
+if (personaMovieDB.count < 10) {
+    console.log("Просмотрено мало фильмов!");
+} else if (personaMovieDB.count <= 30) {
+    console.log("Вы классический зритель!");
+} else {
+    console.log("Вы киноман!");
+}
 
 console.log(personaMovieDB);
